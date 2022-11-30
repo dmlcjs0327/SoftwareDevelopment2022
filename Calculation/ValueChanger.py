115bedcd6f5e5083f9abebe9ad037c2c1cb1cc9c
dmlcjs0327 <rndnjswk123@gmail.com>
2022-11-30 09:20:41 +0900

    할일 배정

--- Calculation/ValueChanger.py
+++ Calculation/ValueChanger.py
@@ -2,10 +2,13 @@
 def change_mm_to_cm(val:int)->int:
     pass
 
-#object_val: (tof값, 윈도우 좌상단 좌표, 윈도우 우하단좌표, screen의 가로픽셀, screen의 세로픽셀)
-#object_coor: (tof값, 물체의 가로길이,물체의 세로길이, 물체 중점의 가로좌표, 물체 중점의 세로좌표)
-#입력값인 object_val을 드론을 원점으로 한 3차원 좌표인 coor로 변환
+
 def chagne_val_to_coor(val:str, screen_width: int, screen_height: int)->tuple:
+    """
+    입력값 - object_val: (tof값[cm], 윈도우 좌상단 좌표[px], 윈도우 우하단좌표[px], screen의 가로픽셀[px], screen의 세로픽셀[px])
+    리턴값 - object_coor: (tof값[cm], 물체중심의 가로좌표[cm], 물체중심의 세로좌표[cm], 물체의 가로길이[cm], 물체의 세로길이[cm])
+    #입력값인 object_val을 드론을 원점으로 한 3차원 좌표인 object_coor로 변환하여 리턴
+    """
     pass
 
 #입력값(cmd)를 tello sdk 명령으로 변환
--- Plan/Planner.py
+++ Plan/Planner.py
@@ -21,9 +21,14 @@ class Planner:
             
     
     
-    #object_coor: (tof값, (물체의 가로길이,물체의 세로길이), (물체 중점의 가로좌표, 물체 중점의 세로좌표)
+    #object_coor: (tof값[cm], 물체중심의 가로좌표[cm], 물체중심의 세로좌표[cm], 물체의 가로길이[cm], 물체의 세로길이[cm])
     #입력값인 object_coor을 회피하는 명령인 avd_cmd를 생성
-    def create_avd_cmd(self,object_coor:tuple)->str:
+    def create_avd_cmd(self,object_coor:tuple)->tuple: 
+        """
+        Tello의 SDK 명령으로 변환하는 함수는 Calculation 패키지의 ValueChanger.change_cmd_for_tello()가 있으므로
+        리턴값은 원하는 형식대로 만들고, 이를 Tello SDK 명령어로 변환하는 ValueChanger.change_cmd_for_tello() 코드도 작성
+        리턴: (이동방향, 해당 방향으로의 이동거리)
+        """
         pass
         
             
